f274d106990a72cf728c27a5f432d987402bb04e
Ziyu Wang <wzyubill1104@gmail.com>
2022-07-21 17:35:20 +0800

    proj3 local passed
    Signed-off-by: Ziyu Wang <wzyubill1104@gmail.com>

--- ddl/table.go
+++ ddl/table.go
@@ -15,6 +15,7 @@ package ddl
 
 import (
 	"fmt"
+
 	"github.com/pingcap/errors"
 	"github.com/pingcap/failpoint"
 	"github.com/pingcap/tidb/infoschema"
@@ -376,6 +377,15 @@ func updateVersionAndTableInfoWithCheck(t *meta.Meta, job *model.Job, tblInfo *m
 func updateVersionAndTableInfo(t *meta.Meta, job *model.Job, tblInfo *model.TableInfo, shouldUpdateVer bool) (
 	ver int64, err error) {
 	// TODO complete this function.
-
+	if shouldUpdateVer {
+		ver, err = updateSchemaVersion(t, job)
+		if err != nil {
+			return ver, errors.Trace(err)
+		}
+	}
+	if tblInfo.State == model.StatePublic {
+		tblInfo.UpdateTS = t.StartTS
+	}
+	err = t.UpdateTable(job.SchemaID, tblInfo)
 	return ver, errors.Trace(err)
 }
